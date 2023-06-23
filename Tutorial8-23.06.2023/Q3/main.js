548b6461e85a109333b820f9e24321292ea6fa7e
Lakshan Rukantha <64830641+LakshanRukantha@users.noreply.github.com>
2023-06-23 16:39:17 +0530

    Tutorial Question 03

---
+++ Tutorial8-23.06.2023/Q3/main.js
@@ -0,0 +1,47 @@
+// const button1 = document.getElementById("btn1");
+// const button2 = document.getElementById("btn2");
+// const button3 = document.getElementById("btn3");
+
+// const changeToRed = () => {
+//   document.bgColor = "red";
+// };
+
+// const changeToGreen = () => {
+//   document.bgColor = "green";
+// };
+
+// const changeToBlue = () => {
+//   document.bgColor = "blue";
+// };
+
+// const changeToWhite = () => {
+//   document.bgColor = "white";
+// };
+
+// button1.addEventListener("mouseover", changeToRed);
+// button1.addEventListener("mouseleave", changeToWhite);
+
+// button2.addEventListener("mouseover", changeToGreen);
+// button2.addEventListener("mouseleave", changeToWhite);
+
+// button3.addEventListener("mouseover", changeToBlue);
+// button3.addEventListener("mouseleave", changeToWhite);
+
+// const changeColor = () => {
+//   button1.addEventListener("mouseover", () => {
+//     document.bgColor = "red";
+//   });
+//   button2.addEventListener("mouseover", () => {
+//     document.bgColor = "green";
+//   });
+//   button3.addEventListener("mousemove", () => {
+//     document.bgColor = "blue";
+//   });
+// };
+
+// changeColor();
+
+const changeColor = (color) => {
+  document.bgColor = color;
+};
+
